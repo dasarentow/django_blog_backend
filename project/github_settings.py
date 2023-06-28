ab2f745c9b0d0c72256253c5ec812816f49d9546
dasarentow <dasarentow@gmail.com>
2023-06-28 14:52:59 +0000

    included badges and licence and resolved email config

--- project/github_settings.py
+++ project/github_settings.py
@@ -120,10 +120,12 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"  # SMTP server address
 EMAIL_PORT = 587  # SMTP server port
 # Use TLS encryption for SMTP
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]  # SMTP username
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]  # SMTP password
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # Password validation
